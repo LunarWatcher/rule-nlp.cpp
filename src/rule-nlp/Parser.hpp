8ee8f9f9dcd3c369c0be4639ab23c5acf170fc6c
Olivia <zoe.i2k1@gmail.com>
2022-04-12 12:25:26 +0200

    Add operator<< and fmt:: stuff
    Damn I love C++

--- src/rule-nlp/Parser.hpp
+++ src/rule-nlp/Parser.hpp
@@ -2,6 +2,19 @@
 
 #include <string>
 #include <vector>
+#if __has_include(<format>)
+#include <format>
+using fmt = std;
+
+#define HAS_FMT
+#elif __has_include(<fmt/format.h>)
+#include <fmt/format.h>
+
+#define HAS_FMT
+#endif
+#ifdef HAS_FMT
+#include <sstream>
+#endif
 
 #include "Rule.hpp"
 #include "SubMatch.hpp"
@@ -15,7 +28,7 @@ public:
     std::vector<Rule> rules;
 
     Parser(const std::vector<Rule>& rules) : rules(rules) {}
-    
+
     ParsedString parseString(const std::string& source);
 };
 
@@ -32,6 +45,34 @@ public:
         : original(original), components(components) {}
 
     std::vector<SubMatch>* operator->();
+    friend std::ostream& operator<<(std::ostream& os, const ParsedString& p);
 };
 
+inline std::ostream& operator<<(std::ostream& os, const ParsedString& p) {
+    os << "ParsedString[" << p.components.size()
+        << ",\n"
+        << "Original: " << p.original << "\n";
+    for (auto& component : p.components) {
+        os << "\tMatch[" << component.category << ", " << component.score << "]: "
+            << std::to_string(component.start) << "-" << component.end
+            << ": " << component.content << "\n";
+    }
+
+    return os << "]";
 }
+
+
+}
+
+#ifdef HAS_FMT
+template <> 
+struct fmt::formatter<rnlp::ParsedString>: fmt::formatter<string_view> {
+
+    template <typename FormatContext>
+    auto format(const rnlp::ParsedString& c, FormatContext& ctx) {
+        std::stringstream s;
+        s << c;
+        return formatter<string_view>::format(s.str(), ctx);
+    }
+};
+#endif
